058cec507a0da968176e13fa964349bad72c4f2c
Luis Henrique <henriqueluis3104@hotmail.com>
2022-04-21 21:17:58 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Quadrado q1 = new Quadrado(4.0);
+
+Console.WriteLine("Area --> {0}", q1.Area);
+Console.WriteLine("Perimetro --> {0}", q1.Perimetro);
+
+Retangulo r1 = new Retangulo(5.0, 10.0);
+
+Console.WriteLine("Area --> {0}", r1.Area);
+Console.WriteLine("Perimetro --> {0}", r1.Perimetro);
